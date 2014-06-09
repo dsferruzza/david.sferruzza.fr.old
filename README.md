93f9d0d508f044468a58477185b566fb9b69d3fd
David Sferruzza <david.sferruzza@gmail.com>
2014-06-09 21:34:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+david.sferruzza.fr
+==================
+
+## How to install
+
+- install haskell platform
+- `cabal update`
+- `cabal install cabal-install`
+- clone this repo
+- `cabal sandbox init`
+- `cabal install --dependencies-only`
+- `cabal build`
+
+To build: `cabal run build`
---
+++ site.hs
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "assets/fonts/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "assets/css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "pages/*.md" $ do
+        route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler

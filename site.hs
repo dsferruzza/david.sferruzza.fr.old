ade5abdfaaf723c36960f21700539118d7b127ca
David Sferruzza <david.sferruzza@gmail.com>
2014-06-12 00:36:04 +0200

    Add basic blog

--- site.hs
+++ site.hs
@@ -3,6 +3,7 @@
 import           GHC.IO.Encoding
 import           Network (withSocketsDo)
 import           Hakyll
+import           Data.Monoid ((<>))
 
 
 --------------------------------------------------------------------------------
@@ -21,10 +22,34 @@ main = do
         route   idRoute
         compile compressCssCompiler
 
+    match "pages/index.md" $ do
+        route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*.md"
+            let pageCtx = listField "posts" postCtx (return posts) <> defaultContext
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" pageCtx
+                >>= relativizeUrls
+
     match "pages/*.md" $ do
         route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+        compile $ do
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
+
+    match "posts/*.md" $ do
+        route   $ setExtension "html"
         compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" <>
+    defaultContext

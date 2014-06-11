fb0e4b787ecdf57bb41e04f0ecc28823f91b7977
David Sferruzza <david.sferruzza@gmail.com>
2014-06-11 23:29:33 +0200

    Handle utf8 content & fix links checking

--- site.hs
+++ site.hs
@@ -1,11 +1,18 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import           GHC.IO.Encoding
+import           Network (withSocketsDo)
 import           Hakyll
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = do
+  setLocaleEncoding utf8
+  setFileSystemEncoding utf8
+  setForeignEncoding utf8
+  withSocketsDo $ hakyll $ do
+
     match "assets/fonts/**" $ do
         route   idRoute
         compile copyFileCompiler

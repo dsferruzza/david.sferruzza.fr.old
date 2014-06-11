c7d340a4374e877972518a9d1aafc981a6c473f2
David Sferruzza <david.sferruzza@gmail.com>
2014-06-12 01:51:18 +0200

    Add tags

--- site.hs
+++ site.hs
@@ -28,7 +28,9 @@ main = do
         route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
         compile $ do
             posts <- fmap (take 5) . recentFirst =<< loadAll postsPattern
-            let pageCtx = listField "posts" postCtx (return posts) <> defaultContext
+            itemTpl <- loadBody "templates/postListItem.html"
+            list <- applyTemplateList itemTpl postCtx posts
+            let pageCtx = constField "title" "Recent posts" <> constField "posts" list <> defaultContext
             pandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" pageCtx
                 >>= relativizeUrls
@@ -41,11 +43,14 @@ main = do
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
+    -- Build tags
+    tags <- buildTags postsPattern (fromCapture "tags/*.html")
+
     -- Posts
     match postsPattern $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html" (tagsCtx tags)
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
@@ -54,9 +59,23 @@ main = do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll postsPattern
-            let archiveCtx = listField "posts" postCtx (return posts) <> defaultContext
+            itemTpl <- loadBody "templates/postListItem.html"
+            list <- applyTemplateList itemTpl postCtx posts
+            let archiveCtx = constField "title" "Blog" <> constField "posts" list <> defaultContext
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/postList.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
+
+    -- Post tags
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged " ++ tag
+        route idRoute
+        compile $ do
+            list <- postList tags pattern recentFirst
+            let archiveCtx = constField "title" title <> constField "posts" list <> defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/postList.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
@@ -72,3 +91,14 @@ postCtx =
     metadataField <>
     dateField "date" "%B %e, %Y" <>
     defaultContext
+
+tagsCtx :: Tags -> Context String
+tagsCtx tags = 
+    tagsField "prettytags" tags <>
+    postCtx
+
+postList :: Tags -> Pattern -> ([Item String] -> Compiler [Item String]) -> Compiler String
+postList tags pattern preprocess' = do
+    postItemTpl <- loadBody "templates/postListItem.html"
+    posts <- preprocess' =<< loadAll pattern
+    applyTemplateList postItemTpl (tagsCtx tags) posts
